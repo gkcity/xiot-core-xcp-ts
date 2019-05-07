78f3e61c16dfa5f674ac4958cb22ceb81dd9a448
ouyang <jxfengzi@gmail.com>
2019-05-07 16:44:26 +0800

    export XcpClient

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export * from './xiot/core/xcp/BinaryFrameCodec';
+export * from './xiot/core/xcp/KeyPair';
+export * from './xiot/core/xcp/XcpClient';
+export * from './xiot/core/xcp/XcpLTSKGetter';
+export * from './xiot/core/xcp/impl/XcpClientBase';
+export * from './xiot/core/xcp/common/XcpAuthenticationType';
+export * from './xiot/core/xcp/common/XcpConstant';
+export * from './xiot/core/xcp/common/XcpFrameCodecType';
+export * from './xiot/core/xcp/common/XcpSessionKey';
+
