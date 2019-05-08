63f5d69cd85d71143faf4e435f0c48b6216665a2
ouyang <jxfengzi@gmail.com>
2019-05-08 16:59:24 +0800

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './xiot/core/xcp/KeyPair';
 export * from './xiot/core/xcp/XcpClient';
 export * from './xiot/core/xcp/XcpLTSKGetter';
 export * from './xiot/core/xcp/impl/XcpClientBase';
+export * from './xiot/core/xcp/impl/XcpClientCipherProductImpl';
 export * from './xiot/core/xcp/common/XcpAuthenticationType';
 export * from './xiot/core/xcp/common/XcpConstant';
 export * from './xiot/core/xcp/common/XcpFrameCodecType';
